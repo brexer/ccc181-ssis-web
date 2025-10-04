81152ee50dc526867773bab653f45ab216fd586d
Bryle Jared Fantilanan <bjrfantilanan@gmail.com>
2025-10-04 23:24:31 +0800

    migrated db

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from database-client import get_connection
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='../frontend')
 cors = CORS(app, origins='*')
 
-@app.route('/')
+@app.route("/api/colleges", methods=["GET"])
+def get_colleges():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT code, name FROM colleges ORDER BY code;")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    colleges = [{"code": r[0], "name": r[1]} for r in rows]
+    return jsonify(colleges)
 def index():
     return render_template('index.html')
 
--- backend/database-client.py
+++ backend/database-client.py
@@ -1,14 +1,16 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
 import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
+from dotenv load load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")  
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
-
-#debug
-print("URL: ", SUPABASE_URL)
-print("KEY: ", SUPABASE_SERVICE_KEY)
+def get_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        pass=os.getenv("DB_PASS"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+    return conn
