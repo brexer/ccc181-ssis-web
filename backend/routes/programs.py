622c6db187c92ee378abf972a970bc79fc5dee13
Bryle Jared Fantilanan <bjrfantilanan@gmail.com>
2025-10-13 00:03:24 +0800

    fix: college codes not added

--- backend/routes/programs.py
+++ backend/routes/programs.py
@@ -20,6 +20,7 @@ def add_program():
         data = request.get_json()
         code = data.get("code")
         name = data.get("name")
+        college_code = data.get("college_code")
 
         if not code or not name:
             return jsonify({"error": "Both code and name are required"}), 400
@@ -34,9 +35,14 @@ def add_program():
                 if cursor.fetchone():
                     return jsonify({"error": f"Program name '{name}' already exists"}), 400
                 
+                if college_code:
+                    cursor.execute("SELECT 1 FROM colleges WHERE code = %s;", (college_code,))
+                    if not cursor.fetchone():
+                        return jsonify({"error": f"College code '{college_code}' does not exist"}), 400
+                
                 cursor.execute(
-                    "INSERT INTO programs (code, name) VALUES (%s, %s) RETURNING code, name;",
-                    (code, name),
+                    "INSERT INTO programs (code, name, college_code) VALUES (%s, %s, %s) RETURNING code, name, college_code;",
+                    (code, name, college_code),
                 )
                 new_program = cursor.fetchone()
             conn.commit()
@@ -59,11 +65,12 @@ def delete_program(code):
         return jsonify({"error": str(e)}), 500
     
 @programs_bp.route("/programs/<code>", methods=["PUT"])
-def update_college(code):
+def update_program(code):
     try:
         data = request.get_json()
         new_code = data.get("code")
         new_name = data.get("name")
+        new_college_code = data.get("college_code")
 
         if not new_code or not new_name:
             return jsonify({"error": "Both code and name are required"}), 400
@@ -87,9 +94,14 @@ def update_college(code):
                 if cursor.fetchone():
                     return jsonify({"error": f"Program name '{new_name}' already exists"}), 400
 
+                if new_college_code:
+                    cursor.execute("SELECT 1 FROM colleges WHERE code = %s;", (new_college_code,))
+                    if not cursor.fetchone():
+                        return jsonify({"error": f"College code '{new_college_code}' does not exist"}), 400
+
                 cursor.execute(
-                    "UPDATE programs SET code = %s, name = %s WHERE code = %s RETURNING code, name;",
-                    (new_code, new_name, code),
+                    "UPDATE programs SET code = %s, name = %s, college_code = %s WHERE code = %s RETURNING code, name, college_code;",
+                    (new_code, new_name, new_college_code, code),
                 )
                 updated_program = cursor.fetchone()
             conn.commit()
